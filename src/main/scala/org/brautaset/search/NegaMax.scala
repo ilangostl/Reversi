fbd2aa8b3b7b4b3a7561d15f70df05a24819d1ea
Stig Brautaset <stig@brautaset.org>
2012-11-10 20:28:26 +0000

    Covariant NegaMax (?)

--- src/main/scala/org/brautaset/search/NegaMax.scala
+++ src/main/scala/org/brautaset/search/NegaMax.scala
@@ -7,16 +7,15 @@ package org.brautaset.search
  * Time: 08:15
  * To change this template use File | Settings | File Templates.
  */
-/*
-class NegaMax extends Search[M <: Move, S <: State] {
+object NegaMax extends Search[Move] {
 
-  private def negamax(state: S, ply: Int): Int =
+  private def negamax(state: State[Move], ply: Int): Int =
     if (ply <= 0)
       state.fitness
     else
       state.legalMoves.map( m => -negamax(state.successor(m), ply - 1)).max
 
-  def search(state: S, ply: Int) = {
+  def search(state: State[Move], ply: Int) = {
     val groups = state.legalMoves.groupBy(m => negamax(state.successor(m), ply - 1))
     val max = groups.keys.max
     println(groups + " => " + max)
@@ -24,4 +23,3 @@ class NegaMax extends Search[M <: Move, S <: State] {
   }
 
 }
-*/
