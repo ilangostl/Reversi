d2d30d1f544bc399a7e6ee510eb08f325f5007a9
Stig Brautaset <stig@brautaset.org>
2013-03-19 00:34:23 +0000

    Board knows the legal moves now

--- src/main/scala/org/brautaset/reversi/Board.scala
+++ src/main/scala/org/brautaset/reversi/Board.scala
@@ -1,5 +1,8 @@
 package org.brautaset.reversi
 
+import annotation.tailrec
+import org.brautaset.reversi.Location.Direction
+
 object Board {
 
   val columns = 8
@@ -30,5 +33,32 @@ case class Board(playerTurn: Player, grid: Map[Location,Player]) {
 
   lazy val unoccupiedNeighboursToOpponent =
     locationsHeldByOpponent.flatMap(_.neighbours).filter(isOnBoard(_)) -- occupiedlocations
+
+  def isLegalMove(location: Location): Boolean = {
+
+    @tailrec
+    def inner(l: Location, d: Direction): Boolean =
+      if (locationsHeldByOpponent.contains(l))
+        inner(l.moveBy(d), d)
+      else
+        occupiedlocations.contains(l)
+
+    @tailrec
+    def outer(loc: Location, d: List[Direction]): Boolean =
+      if (d.isEmpty)
+        false
+      else {
+        val next = loc.moveBy(d.head)
+        if (locationsHeldByOpponent.contains(next) && inner(next, d.head))
+          true
+        else
+          outer(loc, d.tail)
+      }
+
+    outer(location, Location.directions.toList)
+  }
+
+  lazy val legalMoves =
+    unoccupiedNeighboursToOpponent.filter(isLegalMove(_))
 
 }
