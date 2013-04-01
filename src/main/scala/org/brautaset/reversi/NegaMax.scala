ff911ddae26203503a098fcd6b88e051ba2a4ecb
Stig Brautaset <stig@brautaset.org>
2013-04-01 09:53:22 +0100

    Add simple Negamax & Spec

---
+++ src/main/scala/org/brautaset/reversi/NegaMax.scala
@@ -0,0 +1,24 @@
+package org.brautaset.reversi
+
+import fitness.Fitness
+
+object Negamax {
+
+  def negamax(fitness: Fitness)(board: Board, plyLeft: Int) = {
+    require(!board.isFinished)
+    require(plyLeft > 0)
+
+    def iter(board: Board, plyLeft: Int): Int =
+      if (board.isFinished)
+        -board.finishScore
+      else if (plyLeft <= 0)
+        -fitness.fitness(board)
+      else
+        board.legalMoves.map(m => m -> -iter(board.successor(m), plyLeft - 1)).toMap.maxBy(_._2)._2
+
+    val scores = board.legalMoves.map(m => m -> iter(board.successor(m), plyLeft - 1)).toMap
+    // println(scores)
+    scores.maxBy(_._2)._1
+  }
+
+}
---
+++ src/test/scala/org/brautaset/reversi/NegamaxSpec.scala
@@ -0,0 +1,65 @@
+package org.brautaset.reversi
+
+import fitness.{Capture, Fitness}
+import org.scalatest.matchers.MustMatchers
+import org.scalatest.WordSpec
+
+class NegamaxSpec extends WordSpec with MustMatchers {
+
+  val fit = new Fitness with Capture {
+    def fitness(board: Board) = captureFitness(board)
+  }
+
+  val winningMoves = List(
+    Location(4, 5),
+    Location(5, 3),
+    Location(4, 2),
+    Location(5, 5),
+    Location(6, 4),
+    Location(3, 5),
+    Location(4, 6),
+    Location(5, 4),
+    Location(2, 4))
+
+  def iter(board: Board, moves: List[Location]): Board =
+    if (moves.isEmpty) board
+    else iter(board.successor(moves.head), moves.tail)
+
+
+  def nm(board: Board, ply: Int) = Negamax.negamax(fit)(board, ply)
+
+  "negamax" should {
+
+    "not accept finished boards" in {
+      intercept[IllegalArgumentException] {
+        nm(Board(X, Map()), 1)
+      }
+    }
+
+    "not accept ply-left of 0 or less" in {
+      intercept[IllegalArgumentException] {
+        nm(Board(), 0)
+      }
+    }
+
+    "find a move from initial board at ply 1" in {
+      nm(Board(), 1) must be (Location(4, 5))
+    }
+
+    "find a move from initial board at ply 2" in {
+      nm(Board(), 2) must be (Location(4, 5))
+    }
+
+    "find winning move at ply 1" in {
+      val board = iter(Board(), winningMoves.take(8))
+      nm(board, 1) must be (winningMoves(8))
+    }
+
+    "avoid losing move at ply 2" in {
+      val board = iter(Board(), winningMoves.take(7))
+      nm(board, 2) must not be (winningMoves(7))
+    }
+
+  }
+
+}
