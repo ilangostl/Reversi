7e89f3a20978da5fcf5aaf1f330a361e669342a8
Stig Brautaset <stig@brautaset.org>
2013-04-05 04:26:22 +0100

    slight search optimisation in case of a single top-level move available

--- src/main/scala/org/brautaset/reversi/NegaMax.scala
+++ src/main/scala/org/brautaset/reversi/NegaMax.scala
@@ -14,9 +14,11 @@ object Negamax {
       else
         board.legalMoves.map(m => m -> -iter(board.successor(m), plyLeft - 1)).toMap.maxBy(_._2)._2
 
-    val scores = board.legalMoves.par.map(m => m -> iter(board.successor(m), plyLeft - 1)).toMap
-    // println(scores)
-    scores.maxBy(_._2)._1
+    val moves = board.legalMoves
+    if (moves.tail.isEmpty)
+      moves.head
+    else
+      moves.par.map(m => m -> iter(board.successor(m), plyLeft - 1)).toMap.maxBy(_._2)._1
   }
 
 }
