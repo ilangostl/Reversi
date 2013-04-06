7a3857fe3b5ec8c3d4b637be5db2758a7a16a347
Stig Brautaset <stig@brautaset.org>
2013-04-06 21:34:24 +0100

    more idiomatic

--- src/main/scala/org/brautaset/reversi/Negamax.scala
+++ src/main/scala/org/brautaset/reversi/Negamax.scala
@@ -10,7 +10,7 @@ case class Negamax(fitness: Fitness, ply: Int) {
 
     def iter(board: Board, plyLeft: Int): Double =
       if (board.isFinished || plyLeft <= 0)
-        -fitness.fitness(board)
+        fitness.fitness(board)
       else
         board.legalMoves.map(m => -iter(board.successor(m), plyLeft - 1)).max
 
@@ -18,7 +18,7 @@ case class Negamax(fitness: Fitness, ply: Int) {
     if (moves.tail.isEmpty)
       moves.head
     else
-      moves.par.map(m => m -> iter(board.successor(m), ply - 1)).toMap.maxBy(_._2)._1
+      moves.par.map(m => m -> -iter(board.successor(m), ply - 1)).toMap.maxBy(_._2)._1
   }
 
 }
